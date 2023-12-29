bdb4c14816852d42a889d3c45c977c53626bdf14
Christian Blanco <88564832+KritianWhite@users.noreply.github.com>
2023-12-29 05:48:11 -0600

    [ADD] CAMERA READY

--- camera-react/src/App.js
+++ camera-react/src/App.js
@@ -1,4 +1,6 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect } from "react";
+
+import "./App.css";
 
 function App() {
   const videoRef = useRef(null);
@@ -7,26 +9,32 @@ function App() {
   useEffect(() => {
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
       const constraints = { video: true };
-      navigator.mediaDevices.getUserMedia(constraints)
-        .then(stream => {
-          videoRef.current.srcObject = stream;
-          videoRef.current.play();
+      navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then((stream) => {
+          const video = videoRef.current;
+          video.srcObject = stream;
+
+          // Esperar a que los metadatos del video se carguen antes de reproducir
+          video.onloadedmetadata = () => {
+            video.play();
+          };
         })
-        .catch(err => console.error("Error: " + err));
+        .catch((err) => console.error("Error: " + err));
     }
   }, []);
 
   const capturePhoto = () => {
-    const context = canvasRef.current.getContext('2d');
+    const context = canvasRef.current.getContext("2d");
     context.drawImage(videoRef.current, 0, 0, 640, 480);
 
     // Convertir la imagen del canvas a formato de imagen (por ejemplo, PNG)
-    const imageDataURL = canvasRef.current.toDataURL('image/png');
+    const imageDataURL = canvasRef.current.toDataURL("image/png");
 
     // Crear un elemento <a> para descargar la imagen
-    const downloadLink = document.createElement('a');
+    const downloadLink = document.createElement("a");
     downloadLink.href = imageDataURL;
-    downloadLink.download = 'captura.png'; // Nombre por defecto para la imagen
+    downloadLink.download = "captura.png"; // Nombre por defecto para la imagen
 
     // Simular un click para iniciar la descarga
     downloadLink.click();
@@ -36,7 +44,12 @@ function App() {
     <div>
       <video ref={videoRef} width="640" height="480" autoPlay />
       <button onClick={capturePhoto}>Capturar Foto</button>
-      <canvas ref={canvasRef} width="640" height="480" style={{ display: 'none' }}/>
+      <canvas
+        ref={canvasRef}
+        width="640"
+        height="480"
+        style={{ display: "none" }}
+      />
     </div>
   );
 }
